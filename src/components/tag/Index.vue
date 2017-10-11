a4e46bb6f746a2a3cbdddd2dfc66aada401804b0
xiaojieluo <xiaojieluoff@gmail.com>
2017-10-11 13:04:37 +0800

    add tag.index components

---
+++ src/components/tag/Index.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div class="user-indexssss">
+        Welcome!cacascassca
+        scaca
+        casc
+        {{username}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tag',
+    data(){
+        return {
+            username: this.$route.params.tagname
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
